794af681cef3c488351a1c599d67c62e9cfb8096
Jhulli Melo <jhullimmendes@gmail.com>
2022-04-03 18:28:31 -0300

    Processando XML no Serializador

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,14 +1,34 @@
-const { application } = require('express')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
   json(dados) {
     return JSON.stringify(dados)
   }
 
+  xml(dados) {
+    let tag = this.tagSingular
+
+    if (Array.isArray(dados)) {
+      tag = this.tagPlural
+      dados = dados.map(item => {
+        return {
+          [this.tagSingular]: item
+        }
+      })
+    }
+    return jsontoxml({ [tag]: dados })
+  }
+
   serializar(dados) {
+    dados = this.filtrar(dados)
+
     if (this.contentType === 'application/json') {
-      return this.json(this.filtrar(dados))
+      return this.json(dados)
+    }
+
+    if (this.contentType === 'application/xml') {
+      return this.xml(dados)
     }
 
     throw new ValorNaoSuportado(this.contentType)
@@ -45,6 +65,8 @@ class SerializadorFornecedor extends Serializador {
     this.camposPublicos = ['id', 'empresa', 'categoria'].concat(
       camposExtras || []
     )
+    this.tagSingular = 'fornecedor'
+    this.tagPlural = 'fornecedores'
   }
 }
 
@@ -53,6 +75,8 @@ class SerializadorErro extends Serializador {
     super()
     this.contentType = contentType
     this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
+    this.tagSingular = 'erro'
+    this.tagPlural = 'erros'
   }
 }
 
@@ -60,5 +84,5 @@ module.exports = {
   Serializador: Serializador,
   SerializadorFornecedor: SerializadorFornecedor,
   SerializadorErro: SerializadorErro,
-  formatosAceitos: ['application/json']
+  formatosAceitos: ['application/json', 'application/xml']
 }
